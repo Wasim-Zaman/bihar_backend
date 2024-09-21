d230aeee2ebca201ed8ced304ce2e4a9e22c9881
Wasim Zaman <wasimxaman13@gmail.com>
2024-09-21 13:23:07 +0500

    Refactor epicUser routes to use consistent quotation marks and spacing

--- routes/epicUser.js
+++ routes/epicUser.js
@@ -1,36 +1,32 @@
-const express = require("express");
-const { uploadSingle } = require("multermate");
+const express = require('express');
+const { uploadSingle } = require('multermate');
 
-const controller = require("../controllers/epicUser");
-const isAuth = require("../middleware/isAuth");
-const isAdmin = require("../middleware/isAdmin");
+const controller = require('../controllers/epicUser');
+const isAuth = require('../middleware/isAuth');
+const isAdmin = require('../middleware/isAdmin');
 
 const router = express.Router();
 
-router.post("/v1/user", controller.createUser);
+router.post('/v1/user', controller.createUser);
 
 // Route for user login
-router.post("/v1/login", controller.login);
+router.post('/v1/login', controller.login);
 
-router.post("/v1/register", isAuth, controller.register);
+router.post('/v1/register', isAuth, controller.register);
 
-router.post("");
+router.post('');
 
 // Update user
-router.put(
-  "/v1/updateUser",
-  uploadSingle({ filename: "image" }),
-  controller.updateUser
-);
+router.put('/v1/updateUser', isAuth, uploadSingle({ filename: 'image' }), controller.updateUser);
 
-router.get("/v1/users", controller.getUsers);
+router.get('/v1/users', controller.getUsers);
 
-router.get("/v1/users/me", isAuth, controller.getUser);
+router.get('/v1/users/me', isAuth, controller.getUser);
 
-router.delete("/v1/users/:id", isAdmin, controller.deleteUser);
+router.delete('/v1/users/:id', isAdmin, controller.deleteUser);
 
-router.put("/v1/users/status/:id", isAdmin, controller.updateStatus);
+router.put('/v1/users/status/:id', isAdmin, controller.updateStatus);
 
-router.get("/v1/users/search", controller.searchUsers);
+router.get('/v1/users/search', controller.searchUsers);
 
 module.exports = router;
